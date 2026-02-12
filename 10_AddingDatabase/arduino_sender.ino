f677e281aa6e6c4dac60aa6eda5624f4fdfd1651
Ismail Hossain <ismail.hossain@centria.fi>
2026-02-12 22:14:02 +0200

    Add Arduino sender to read and send sensor values

--- 10_AddingDatabase/arduino_sender.ino
+++ 10_AddingDatabase/arduino_sender.ino
@@ -1,1 +1,14 @@
+// Exercise 10 - Adding Database
+// Arduino sender: sends sensor value every 1 second via Serial
 
+const int SENSOR_PIN = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(SENSOR_PIN);  // 0–1023
+  Serial.println(sensorValue);               // send value to computer
+  delay(1000);
+}
